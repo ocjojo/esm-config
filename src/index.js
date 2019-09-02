e7f66d39037af86f16d79d4c754c8709abdca482
Lukas Ehnle <lukas@owlabout.de>
2019-09-02 18:10:56 +0200

    allow configuration other than arrays, allow defaultConfiguration

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ const rollup = require("rollup")
 const path = require("path")
 
 // adapted from https://github.com/rollup/rollup/blob/def3ae2b4d2e5fde0f28d5ff1bf92ab9a2899e28/bin/src/run/loadConfigFile.ts
-export default async function esmConfig(configFile, options = {}) {
+export default async function esmConfig(configFile, defaultConfig = {}) {
 	const bundle = await rollup.rollup({
 		external: id =>
 			(id[0] !== "." && !path.isAbsolute(id)) ||
 			id.slice(-5, id.length) === ".json",
 		input: configFile,
 		treeshake: false
-	})
+	}).catch(() => {})
+
+	if(!bundle) return defaultConfig
+
 	const {
 		output: [{ code }]
 	} = await bundle.generate({
@@ -33,16 +36,9 @@ export default async function esmConfig(configFile, options = {}) {
 
 	let configs = configFileContent
 	if (typeof configFileContent === "function") {
-		configs = await configFileContent(options)
+		configs = await configFileContent(defaultConfig)
 	}
 	require.extensions[".js"] = defaultLoader
 
-	if (Object.keys(configs).length === 0) {
-		throw new Error(
-			"Config file must export an options object, or an array of options objects" +
-				"\nhttps://rollupjs.org/guide/en/#configuration-files"
-		)
-	}
-
-	return Array.isArray(configs) ? configs : [configs]
+	return configs
 }
